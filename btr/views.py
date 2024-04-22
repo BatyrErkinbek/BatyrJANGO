7eb31ace9edaa4dbc9b20d2428feb754f1d48756
Batyr VIP <batyr.erkinbek@mail.ru>
2024-04-22 16:51:17 +0500

    lab10correct

--- btr/views.py
+++ btr/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'templates/btr/indd.html')
+    return render(request, 'btr/indd.html')
 
 
 def about(request):
-    return HttpResponse("<h4>o nas</h4>")
+    return render(request,'btr/about.html')
